1d3982c549bd8d3d44a3ba712b68bab2c3bd1bf7
慕小白 <zpf12345678910@gmail.com>
2017-04-27 14:42:16 +0800

    解决打包不能正常访问的问题

--- spring-boot/src/main/java/config/JerseyConfig.java
+++ spring-boot/src/main/java/config/JerseyConfig.java
@@ -9,8 +9,17 @@
 
 package config;
 
+import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
+import org.springframework.core.type.filter.RegexPatternTypeFilter;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ClassUtils;
 import org.springframework.web.filter.RequestContextFilter;
 
 /**
@@ -27,7 +36,13 @@ import org.springframework.web.filter.RequestContextFilter;
 public class JerseyConfig extends ResourceConfig{
     public JerseyConfig() {
         register(RequestContextFilter.class);
-        packages("rest");
+       // packages("rest");
+        ClassPathScanningCandidateComponentProvider provider = new ClassPathScanningCandidateComponentProvider(false);
+        provider.addIncludeFilter(new RegexPatternTypeFilter(Pattern.compile(".*")));
+        Set<BeanDefinition> candidateComponents = provider.findCandidateComponents("rest");
+        for (BeanDefinition candidateComponent : candidateComponents) {
+          register(ClassUtils.resolveClassName(candidateComponent.getBeanClassName(), ClassUtils.getDefaultClassLoader()));
+        }
 //        register(LoggingFilter.class);
     }
 }
